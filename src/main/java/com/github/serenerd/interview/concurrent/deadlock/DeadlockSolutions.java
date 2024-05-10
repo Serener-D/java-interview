6199ce3cf413e4b2ed5a90d34f26792d5d5178ed
Denis Pronin <de.pronin@gmail.com>
2024-05-10 14:43:11 +0500

    updated DeadlockSolutions

--- src/main/java/com/github/serenerd/interview/concurrent/deadlock/DeadlockSolutions.java
+++ src/main/java/com/github/serenerd/interview/concurrent/deadlock/DeadlockSolutions.java
@@ -1,9 +1,37 @@
 package com.github.serenerd.interview.concurrent.deadlock;
 
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class DeadlockSolutions {
+
+    public static void deadlock() {
+        Lock lock1 = new ReentrantLock();
+        Lock lock2 = new ReentrantLock();
+
+        Executor executor = Executors.newFixedThreadPool(2);
+        executor.execute(() -> {
+            System.out.println(Thread.currentThread().getName() + "locking lock 1");
+            lock1.lock();
+            System.out.println(Thread.currentThread().getName() + "locking lock 2");
+            lock2.lock();
+
+            lock2.unlock();
+
+            lock1.unlock();
+        });
+        executor.execute(() -> {
+            System.out.println(Thread.currentThread().getName() + "locking lock 2");
+            lock2.lock();
+            System.out.println(Thread.currentThread().getName() + "locking lock 1");
+            lock1.lock();
+            lock1.unlock();
+
+            lock2.unlock();
+        });
+    }
 
     private static class Example {
         private static final Lock lock1 = new ReentrantLock();
